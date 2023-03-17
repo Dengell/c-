a9cda50cd0ebfd90b36b9808694fe4ef80e10c39
Dengell <9680004050@mail.ru>
2023-03-17 15:19:45 +0300

    Выполнил Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаетпроизведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+
+int Factorial(int num)
+{
+    int product = 1;
+    for(int i = 2; i <= num; i++)
+    {
+        product = product * i;
+        //sum += i;
+    }
+    return product;
+}
+
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+
+Console.WriteLine($"Сумма чисел от 1 до {number} = {factorial}");
