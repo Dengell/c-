c6fc7cd7644b86f6523caf5efea921cfd0cd8403
Dengell <9680004050@mail.ru>
2023-03-24 12:25:10 +0300

    Выполнил Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] FiboArray(int number)
+{
+    int[] array = new int[number];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+       array[i] = array[i-1] + array[i -2]; 
+    }
+    return array;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 1) PrintArray(FiboArray(num));
+else Console.WriteLine("Количество элементов должно быть не меньше двух, для вычисления чисел Фибоначи");
